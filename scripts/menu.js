29f1772f6aa8c0ef582d156e290559b28e931171
夷立松 <yilisong@192.168.0.107>
2018-03-18 18:28:59 +0800

    update

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,5 +1,9 @@
 function menuClick(e){
     var lis = e.parentNode.childNodes
+    if(e.innerText == '消防管理') {
+      window.open("http://118.25.14.190:5500")
+      return
+    } 
     lis.forEach((item) => {
       item.className = ""
     })
